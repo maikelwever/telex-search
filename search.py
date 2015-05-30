804c0cc56170e7b518a5d6a6790a23d658096a05
Akshay S Dinesh <asdofindia@gmail.com>
2015-05-30 10:19:23 +0530

    add code

--- search.py
+++ search.py
@@ -2,8 +2,7 @@ import plugintypes
 import tgl
 from telegrambot.utils.decorators import group_only
 import requests
-from bs4 import BeautifulSoup
-
+import json
 
 class SearchPlugin(plugintypes.TelegramPlugin):
     """
@@ -24,3 +23,25 @@ class SearchPlugin(plugintypes.TelegramPlugin):
 
     @group_only
     def google(self, msg, matches):
+        '''Returns the link and the description of the first result from a google search
+        '''
+        #query = raw_input ( 'Query: ' )
+        query=matches.group(1)
+        # print "going to google %s" % query
+        query = urllib.urlencode ( { 'q' : query } )
+        params = {
+            'q': query,
+        }
+        response = requests.get('http://ajax.googleapis.com/ajax/services/search/web?v=1.0&', params = params).text
+        jresults = json.loads ( response )
+        results = jresults [ 'responseData' ] [ 'results' ]
+        returnval=""
+        for result in results:
+            title = result['title']
+            url = result['url']   # was URL in the original and that threw a name error exception
+            #print ( title + '; ' + url )
+            title=title.translate({ord(k):None for k in u'<b>'})
+            title=title.translate({ord(k):None for k in u'</b>'})
+            returnval += title + ' ; ' + url + '\n'
+
+        return returnval
